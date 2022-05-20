71470fba75f88bcb2ace471f3947bbfd18684c87
Jhon Pardo <jhpardo99@misena.edu.co>
2022-05-20 00:27:56 -0500

    Reto 10 avance pendiente manejo de error de tamaño array

--- src/Reto10.cs
+++ src/Reto10.cs
@@ -6,8 +6,10 @@ public class Reto10
     #nullable disable
     string posProd;
     bool isPlaying;
+    double cambio;
 
-    string[,] precio2 = new string[4,4];
+    string[,] productos = new string[2,2];
+    double[,] precios = new double[2,2];
 
     double[,] precio = new double[4,4]{{1700, 850, 1300, 900},
       {400, 900, 1250, 1100},
@@ -18,6 +20,8 @@ public class Reto10
       {"Chocolatina", "Oreo Paquete", "Taco Saltina", "Club Social"},
       {"Yogurt Alpina", "SpeedMax Litro", "Vive100 Litro", "RedBull Original"},
       {"Quesadillo Org", "Jamon Ahumado", "Bocadillo ", "Ensalada Atún"}};
+
+    
 
 
     public void iniciar()  {
@@ -30,42 +34,47 @@ public class Reto10
          if (proc == 1)
         {
             rellenar();
+            comprar(true);
         }else{
-            comprar();
+            comprar(false);
         }
     }
 
     void rellenar(){
 
         //Rellenando productos
-        for (int i = 0; i < product.GetLength(0); i++)
+        for (int i = 0; i < productos.GetLength(0); i++)
         {
-            Console.WriteLine(" ");
-            for (int j = 0; j < product.GetLength(0); j++)
+            for (int j = 0; j < productos.GetLength(0); j++)
             {
                 Console.Write($"Ingrese el producto de la posición {i}{j} \t");
-                precio2[i,j] = Console.ReadLine();
-                
+                productos[i,j] = Console.ReadLine(); 
             }
-
-            Console.WriteLine(" ");
-           /* for (int j = 0; j < precio.GetLength(1); j++)
-            {
-                Console.Write($"Ingrese el precio del producto {precio2[i,j]}\t");
-            }
-            Console.WriteLine("");*/
         }
 
-        
+        //Rellenando precios
+        for (int i = 0; i < precios.GetLength(0); i++)
+        {
+           for (int j = 0; j < precios.GetLength(1); j++)
+            {
+                Console.Write($"Ingrese el precio del producto {productos[i,j]}\t");
+                precios[i,j] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
     }
 
-    void comprar(){
-        Console.WriteLine("INGRESA SU DINERO");
+    void comprar(bool proceso){
+        Console.WriteLine("INGRESA SU DINERO PARA COMPRAR");
         player.TotalDinero = Convert.ToDouble(Console.ReadLine());
 
         do
         {
-            maquina();
+            if (proceso)
+            {
+                maquina(productos, precios);     
+            } else {
+                maquina(product, precio);
+            } 
 
             if (isPlaying){
                 Console.WriteLine("\nMarque (X) para cancelar o marque una posición correcta...");
@@ -75,7 +84,7 @@ public class Reto10
 
             posProd = Console.ReadLine();
 
-            if (posProd.Equals("X")){
+            if (posProd.Equals("X") || posProd.Equals("x") ){
                 isPlaying = false; 
             }else if (posProd.Length != 2){
                 isPlaying = true;
@@ -84,36 +93,73 @@ public class Reto10
             }
 
         } while (isPlaying);
-        calcPos(posProd);
+        calcPos(posProd, proceso);
     }
     
-    void calcPos(string prod){
+    void calcPos(string prod, bool proceso){
+
+        string compra;
+        double costoCompra;
+
+        Console.WriteLine($"Tamaño 1: {productos.GetLength(0)} - 2: {productos.GetLength(0)}");
+
+/*
+        int[,] a = new int[17, 2];
+            int i = a.Rank;
+            for(int x = 0; x < i; x++)
+            {
+                Console.WriteLine(a.GetLength(x));
+            }
+*/
+
+//Validar error si da una longitud de matrix equivocada con la 
+//linea 104 donde se tiene que confirmar los 2 numeros que ingresan sean menores o iguales a cada uno del tamaño
+
+
+
+
+
+
         if (prod.Length == 2)
         {
-            Console.WriteLine($"{prod[0]}  y {prod[1]}");
+            if (proceso == true){
+                compra = productos[(int)Char.GetNumericValue(prod[0]),(int)Char.GetNumericValue(prod[1])];
+                costoCompra = Convert.ToDouble(precios[(int)Char.GetNumericValue(prod[0]),(int)Char.GetNumericValue(prod[1])]);
+            }else {
+                compra = product[(int)Char.GetNumericValue(prod[0]),(int)Char.GetNumericValue(prod[1])];
+                costoCompra = Convert.ToDouble(precio[(int)Char.GetNumericValue(prod[0]),(int)Char.GetNumericValue(prod[1])]);
+            }
 
-            //Pendiente calcular saldo con precio del producto
-            Console.WriteLine("Posicion valida");
-        }
+            if (player.TotalDinero > costoCompra)
+            {
+                cambio = player.TotalDinero - costoCompra;
+                Console.WriteLine($"{compra} ¡ADQUIRIDO!");
+                Console.WriteLine($"Su cambio es: {cambio}");
+            } else{
+                Console.WriteLine("Dinero insuficiente. **Vuelva a intentar");
+            }
+        } 
     }
 
-    public void maquina()
+    public void maquina(string[,] pro, double[,] cos)
     {
-        for (int i = 0; i < product.GetLength(0); i++)
+        for (int i = 0; i < pro.GetLength(0); i++)
         {
-
             Console.WriteLine(" ");
-            for (int j = 0; j < product.GetLength(1); j++)
+            for (int j = 0; j < pro.GetLength(1); j++)
             {
-                Console.Write($"{product[i,j]} \t");
+                Console.Write($"Posición {i}{j} \t");
             }
-
             Console.WriteLine(" ");
-            for (int j = 0; j < precio.GetLength(1); j++)
+            for (int j = 0; j < pro.GetLength(1); j++)
             {
-                Console.Write($"Precio: {precio[i,j]} \t");
+                Console.Write($"{pro[i,j]} \t");
             }
-
+            Console.WriteLine(" ");
+            for (int j = 0; j < cos.GetLength(1); j++)
+            {
+                Console.Write($"Precio: {cos[i,j]} \t");
+            }
             Console.WriteLine("");
         }
     }
